1eca9699162099134c86bc50be55e10535dc6c6a
Lynn Cook <lynn.m.cook@gmail.com>
2013-07-10 18:04:07 +1000

    new files

--- 05_Classes_Objects/exercises/ex_secret_number_objects/secret_number.rb
+++ 05_Classes_Objects/exercises/ex_secret_number_objects/secret_number.rb
@@ -16,16 +16,64 @@
 #
 # Main
 #	The main class is where the game begins. This class should tell the player who made it.
+class Main
+
+	def welcome()
+	puts "\nWelcome to the Secret Number Game!\n\tCreated by Lynn\nI hope you enjoy this thrilling game of skill.\n\tThe Rules:\n1. you must guess the secret number, it's a number between 1 and 10,\n2. you only get three guesses, so choose wisely."
+	end
+end
 #	Ask the player for their name and instantiate the Game class.
+class Game
+	
+	def get_name
+		puts "And your name is?\n"
+		@name = gets.chomp
+	end
+
+	def put_name
+		@name
+	end
+
+end
+name = Game.new
+name.get_name
+puts "Good to meet you #{name.put_name}"
+
+
 #
 # Secret_Number
 #   This class should initiate an array of numbers that range between 1 - 10.
 #   Use a method from the array class to select a random element from that array. This random number will be the secret number. 
 #   This way the secret number is also a secret to you.
 #
+class Secret_Number
+
+	def generate
+		@secret_number = 1 + rand(10)
+	end
+
+	def reveal
+		@secret_number
+	end
+end
+
+sn = Secret_Number.new
+sn.generate
+puts "The secret number is #{sn.reveal}."
+puts "What was that again?"
+puts "I said \"The secret number is #{sn.reveal}!\""
+puts "Can you tell me a different secret number?"
+puts "Okay"
+sn.generate
+puts "The new secret number is #{sn.reveal}."
+
 # Player
 #  Should initialize the player's name. 
 #
+class Player
+
+end
+
 # Game
 #	This class holds most of the game logic and should:
 #		Welcome players and inform them of the game rules.
---
+++ 05_Classes_Objects/homework/midterm.rb
@@ -0,0 +1,5 @@
+# Requirements
+# take user input and handle bad input
+# connect to a 3rd party api where the call varies based on the user's input
+# object oriented, instantiate modeled classes in separate files
+# push code on GitHub
