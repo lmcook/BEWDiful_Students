552a06e48c54d17bc20d67347fac8a4259ff6574
Lynn Cook <lynn.m.cook@gmail.com>
2013-07-19 16:16:09 +1000

    midterm

--- 05_Classes_Objects/homework/midterm.rb
+++ 05_Classes_Objects/homework/midterm.rb
@@ -4,66 +4,64 @@
 # object oriented, instantiate modeled classes in separate files
 # push code on GitHub
 
-
-# 2. display currently available story tags
-# class to display story tags
-# 3. ask user which tags they are interested in: individual, set or all
-# class to communicate with user
 # 4. display story info for selected tags
 # class to display selected stories
 # 5. ask user if they'd like to make a new selection, save their current selection to file, or quit
 # class to communicate with user - same as before?
 
 # 1. use digg api
-# class to get digg feed
 
 require 'httpi'
 require 'json'
 
-class GetDiggFeed
-	# get json from digg
+# get json from digg
 
-	def get_digg_feed
-		request = HTTPI::Request.new
-		request.url = "http://digg.com/api/news/popular.json"
-		HTTPI.get(request).body	
-	end
-
-	# parse json to hash
-
-	def parse_digg_feed(raw_response)
-		JSON.parse(raw_response)
-	end
-
+def get_digg_feed
+	request = HTTPI::Request.new
+	request.url = "http://digg.com/api/news/popular.json"
+	HTTPI.get(request).body	
 end
 
-# print story tags to terminal
+# parse json to hash
 
-def print_digg_tags(stories)
-	puts "These are the currently available story tags:"
+def parse_digg_feed(raw_response)
+	JSON.parse(raw_response)
+end
+
+# 2. get story tags and put them in an array, 
+
+def get_tags(stories)
+	current_tags = []
 	stories.each do |story|
-		print_tags(story)
+		current_tags.push(story["content"]["tags"].first["slug"])
+		# tag = story["content"]["tags"]
+	 	# 	tag.each do |x|
+	 	# 		current_tags.push(x["slug"])
 	end
+	current_tags.uniq!
+	current_tags.sort!
+
+	#display currently available story tags
+	puts
+	puts "These are the currently available story tags:"
+	puts current_tags
 	puts
 end
 
-# ask for user input
+# 3. ask user which tag they are interested in: individual, set or all
 
-def user_select
+def user_select(story)
 	puts "Choose a tag!"
 	choice = gets.chomp
 	puts
-end
-
-# print stories to terminal
-
-def print_digg_stories(stories)
-	stories.each do |story|
-		print_digg_story(story)
-		puts "Tags: " + print_tags(story).to_s
-		puts
-		puts
+	# if the user enters a tag from the list, find the associated stories and display them
+	# if the user enters an invalid choice ask them to choose again
+	if choice == "#{story["content"]["tags"].first["slug"]}"
+		puts "Title: #{story["content"]["title"]}"
+	else
+		puts "Please choose a tag in the list."
 	end
+	puts
 end
 
 def print_digg_story(story)
@@ -72,22 +70,39 @@ def print_digg_story(story)
 	puts "Description: #{story["content"]["description"]}"
 	puts "Source: #{story["content"]["domain"]}"
 	puts "URL: #{story["content"]["url"]}"
+	puts "Tag: #{story["content"]["tags"].first["slug"]}"
 end
-
-def print_tags(story)
-	tag = story["content"]["tags"]
-	tag.each do |x|
-		# x["display"], " "
-		print x["display"], " "
-	end
-end
-
 
 raw_response = get_digg_feed
 response = parse_digg_feed(raw_response)
+
+get_tags(response["data"]["feed"])
+user_select(response["data"]["feed"])
 print_digg_stories(response["data"]["feed"])
+
+
+# def print_tags(story)
+# 	tag = story["content"]["tags"]
+# 	tag.each do |x|
+# 		print x["display"], " "
+# 	end
+# end
+
+# print stories to terminal
+
+# def print_digg_stories(stories)
+# 	stories.each do |story|
+# 		print_digg_story(story)
+# 		puts "Tags: " + print_tags(story).to_s
+# 		puts
+# 		puts
+# 	end
+# end
+
+
 # print_digg_tags(response["data"]["feed"])
-# user_select
+
+
 
 
 
