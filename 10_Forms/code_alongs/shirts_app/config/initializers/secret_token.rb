f95f555ff78b98a3d36ac4d5f87ef332e6680a23
Lynn Cook <lynn.m.cook@gmail.com>
2013-07-25 11:40:38 +1000

    shirts_app

---
+++ 10_Forms/code_alongs/shirts_app/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ShirtsApp::Application.config.secret_key_base = '238ba8bed69e71918e0ed930443a9429250d35019e3de62288738f9d3220797fb849c3265c1a2abf7ef31f669dc5cf0180c460918c233ec01add09ef27ddb0b6'
